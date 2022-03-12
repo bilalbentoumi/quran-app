892d528a9e977cc923b15dcf76b0df213ff419a4
bilalbentoumi <bilalbentoumi@gmail.com>
2022-03-12 12:45:00 +0100

    Create the basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import NavBar from './NavBar'
+import Sidebar from './Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <div className="app h-full flex flex-col">
+            <NavBar/>
+            <div className="app-content flex flex-grow">
+                <Sidebar/>
+                <div className="main p-8 flex-grow bg-gray-100 rounded-tl-3xl shadow-inner">
+                    Main
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
---
+++ src/NavBar.jsx
@@ -0,0 +1,12 @@
+import Logo from './Logo'
+
+function NavBar() {
+
+    return (
+        <div className="navbar p-4">
+            <Logo/>
+        </div>
+    )
+}
+
+export default NavBar
